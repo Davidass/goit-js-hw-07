e271686b54b9d0a4b82320a713f1b40b91f5ce27
Davidass <gidrig@gmail.com>
2020-07-19 17:55:51 +0300

    lastStend

--- js/tasks-08.js
+++ js/tasks-08.js
@@ -1,11 +1,10 @@
-import users from './users.js'; // eslint-disable-line
-
-const getUsersWithFriend = (users, friendName) => {
-  const allUsers = users.filter(function(user) {
-    return user.friends.includes(friendName);
-  });
-  return allUsers.map(user => user.name);
-};
-
-console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+// Напиши скрипт создания и очистки коллекции элементов.
+// Пользователь вводит количество элементов в input и нажимает кнопку Создать,
+// после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+// Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.
+// Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
+// Каждый созданный div:
+// Имеет случайный rgb цвет фона
+// Размеры самого первого div - 30px на 30px
+// Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
+// Создай функцию destroyBoxes(), которая очищает div#boxes.
